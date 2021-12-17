9ee3defdeafe8082fe3f2f496c02e8a88a96bb73
stephane <st.rianha@gmail.com>
2021-12-17 17:55:26 +0000

    Creation repository

---
+++ AgenceVoyage/src/main/java/repository/ActiviteRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Activite;
+
+public interface ActiviteRepository extends JpaRepository<Activite, Integer>{
+
+}
