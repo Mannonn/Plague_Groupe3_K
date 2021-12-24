dbf4b92a9d0653778e5b8d5475300aa5c0a509f4
stephane <st.rianha@gmail.com>
2021-12-24 15:18:45 +0000

    test

--- agenceVoyage-back/src/main/java/service/ActiviteService.java
+++ agenceVoyage-back/src/main/java/service/ActiviteService.java
@@ -20,7 +20,17 @@ public class ActiviteService {
 			throw new ActiviteException();
 		}
 		activiteRepo.save(activite);
-	}
+	} 
+	
+	public Activite update(Activite activite) {
+        if (activite.getId() == null) {
+            throw new ActiviteException();
+        }
+        Activite activiteEnBase = activiteRepo.findById(activite.getId()).orElseThrow(ActiviteException::new);
+        creation(activiteEnBase);
+        return activiteRepo.save(activite);
+
+    }
 
 	public void suppression(Activite activite) {
 		Activite activiteEnBase = null;
