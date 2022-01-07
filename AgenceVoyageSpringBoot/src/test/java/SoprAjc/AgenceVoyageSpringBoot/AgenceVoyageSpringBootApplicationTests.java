4cacef41328212974bed5c812dc68c7d2141f70d
Amael <bouchiere.amael@gmail.com>
2022-01-07 12:13:46 +0100

    Springboot

---
+++ AgenceVoyageSpringBoot/src/test/java/SoprAjc/AgenceVoyageSpringBoot/AgenceVoyageSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package SoprAjc.AgenceVoyageSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenceVoyageSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
