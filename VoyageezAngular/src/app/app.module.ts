1c609e0f23a194f400f4f26426efdb38740dd2be
Mannonn <manonj@hotmail.fr>
2022-01-18 17:09:18 +0100

    site tout bô

--- VoyageezAngular/src/app/app.module.ts
+++ VoyageezAngular/src/app/app.module.ts
@@ -10,14 +10,11 @@ import { HomeComponent } from './FrontSite/home/home.component';
 import { routes } from './routes';
 import { FooterComponent } from './FrontSite/footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './FrontSite/nav/nav.component';
-import { NavigationParLeCodeComponent } from './navigation-par-le-code/navigation-par-le-code.component';
 import { MissionsComponent } from './FrontSite/missions/missions.component';
 import { FaqComponent } from './FrontSite/faq/faq.component';
 import { ConnaitreComponent } from './FrontSite/connaitre/connaitre.component';
 import { ActivitesComponent } from './FrontSite/activites/activites.component';
 import { BanniereComponent } from './FrontSite/banniere/banniere.component';
-// import { ConnexionComponent } from './FrontSite/connexion/connexion.component';
 import { ContactComponent } from './FrontSite/contact/contact.component';
 import { InfosComponent } from './FrontSite/infos/infos.component';
 import { InscriptionComponent } from './FrontSite/inscription/inscription.component';
@@ -46,9 +43,7 @@ import { EditPassagerComponent } from './voyageez/passager/edit-passager/edit-pa
 import { EditReservationComponent } from './voyageez/reservation/edit-reservation/edit-reservation.component';
 import { EditTrajetComponent } from './voyageez/trajet/edit-trajet/edit-trajet.component';
 import { EditVaisseauComponent } from './voyageez/vaisseau/edit-vaisseau/edit-vaisseau.component';
-import { Roles } from './Roles';
 import { LoginComponent } from './voyageez/login/login.component';
-import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
@@ -56,14 +51,11 @@ import { NavigationComponent } from './navigation/navigation.component';
     HomeComponent,
     FooterComponent,
     HeaderComponent,
-    NavComponent,
-    NavigationParLeCodeComponent,
     MissionsComponent,
     FaqComponent,
     ConnaitreComponent,
     ActivitesComponent,
     BanniereComponent,
-    // ConnexionComponent,
     ContactComponent,
     InfosComponent,
     InscriptionComponent,
@@ -92,7 +84,6 @@ import { NavigationComponent } from './navigation/navigation.component';
     EditTrajetComponent,
     EditVaisseauComponent,
     LoginComponent,
-    NavigationComponent,
   ],
   imports: [
     BrowserModule,
