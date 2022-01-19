83cd201ad2b8569c1b3f9279f59a04ad1955b0d9
Mannonn <manonj@hotmail.fr>
2022-01-19 23:11:28 +0100

    triage passager selon client abandonné (litsPassa ne se remplit pas) et css pas fait

--- VoyageezAngular/src/app/voyageez/passager/passager.component.ts
+++ VoyageezAngular/src/app/voyageez/passager/passager.component.ts
@@ -1,6 +1,11 @@
+import { ReservationService } from './../../services/reservation.service';
+import { Client } from './../../model/client';
 import { Component, OnInit } from '@angular/core';
 import { Passager } from 'src/app/model/passager';
 import { PassagerService } from 'src/app/services/passager.service';
+import { ClientService } from 'src/app/services/client.service';
+import { Reservation } from 'src/app/model/reservation';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-passager',
@@ -9,16 +14,49 @@ import { PassagerService } from 'src/app/services/passager.service';
 })
 export class PassagerComponent implements OnInit {
   passagers: Passager[] = [];
-  constructor(private passagerService: PassagerService) {}
+  listPassa: Passager[] = [];
+  client: Client = new Client();
+  reservations: Reservation[] = [];
+  affichOK: boolean = false;
+  constructor(
+    private passagerService: PassagerService,
+    private clientService: ClientService,
+    private reservationService: ReservationService
+  ) {}
 
   ngOnInit(): void {
     this.initPassager();
+    this.initClient();
   }
 
   initPassager() {
     this.passagerService.getAll().subscribe((result) => {
       this.passagers = result;
     });
+  }
+
+  initClient() {
+    this.clientService
+      .getById(+localStorage.getItem('id')!)
+      .subscribe((result) => {
+        this.client = result;
+      });
+  }
+
+  listPassagers() {
+    for (let r of this.reservations) {
+      for (let p of r.passagers!) {
+        if (this.client.id! == p.client?.id!) {
+          console.log(p);
+          this.listPassa.push(p);
+        }
+      }
+    }
+    this.affichOK = true;
+  }
+
+  affichTableau() {
+    this.listPassagers();
   }
 
   delete(id: number) {
